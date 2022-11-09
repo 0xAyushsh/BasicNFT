2fb2e3dac6710726f079f9885dd5a5ed81ca1110
Lucifer <this.ayushsh@gmail.com>
2022-11-10 00:29:53 +0530

    BasicNFT

---
+++ deploy/01-deploy-nft.ts
@@ -0,0 +1,42 @@
+import { DeployFunction } from "hardhat-deploy/dist/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat-config"
+
+
+const deployBasicNFT: DeployFunction = async (
+    hre: HardhatRuntimeEnvironment
+) => {
+
+    //Getting infromtaion from hardhat-config.ts file
+    const { getNamedAccounts, network, ethers, deployments } = hre
+
+    //Getting deploy and log function from deployments 
+    const { deploy, log } = deployments
+
+    //Getting deployer config from named accounts config in hardhat-config.ts file
+    const { deployer } = await getNamedAccounts()
+
+    const args: any[] = [1,1]
+
+    log("Start Deployment of Auction contract")
+
+    const confirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+        //Deploying BasicNFT.sol smart contract using deploy function
+    const basicNFT = await deploy("BasicNFT", {
+        //Account from which we want to do the transaction
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: confirmations,
+    })
+
+    log(`Deployment of BasicNFT contract Successful!`)
+
+}
+
+
+export default deployBasicNFT
+deployBasicNFT.tags = ["all", "basicNFT"]
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,41 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: ".env" });
+// require("@nomicfoundation/hardhat-toolbox");
+// require("dotenv").config({ path: ".env" });
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-module.exports = {
-  solidity: "0.8.9",
-  networks: {
-    goerli: {
-      url: QUICKNODE_HTTP_URL,
-      accounts: [PRIVATE_KEY],
+// module.exports = {
+//   solidity: "0.8.9",
+//   networks: {
+//     goerli: {
+//       url: QUICKNODE_HTTP_URL,
+//       accounts: [PRIVATE_KEY],
+//     },
+//   },
+// };
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-deploy"
+import "dotenv/config"
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [{ version: "0.8.9" }],
     },
-  },
-};
+    defaultNetwork: "goerli",
+    networks: {
+        goerli: {
+            url: QUICKNODE_HTTP_URL,
+            chainId: 5,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+}
+
+export default config
